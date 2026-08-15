0c0874de8dd82f01344509bd1fa62569b4626bfe
星灿长风v <2452234@st.shou.edu.cn>
2026-08-15 10:23:46 +0800

    stage

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ PARENT_DIR=$(dirname "$SCRIPT_DIR")
 
 # shellcheck disable=SC2164
 cd "$PARENT_DIR"
-mkdir build && cd build || exit
+mkdir -p build && cd build || exit
 cmake .. -DCMAKE_C_STANDARD=11 -DCMAKE_C_COMPILER=clang
 # shellcheck disable=SC2046
 make -j$(nproc)
